63523c05d9700e52086f3f2d39131c948af4cf01
WrightRickman <wrightrickman@gmail.com>
2023-07-17 11:56:19 -0400

    Added world clock (that updates)

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const navContainer = document.getElementById('nav__list');
 const activeLine = document.getElementById('line--active');
 
-// Append list of cities from JSON
-
+// Fetch list of cities in JSON format
 async function fetchCities() {
   let url = './navigation.json';
 
@@ -14,6 +13,48 @@ async function fetchCities() {
   }
 }
 
+function setTime(section) {
+  const timezones = {
+    'cupertino': {
+      'timezone': 'America/Los_Angeles'
+    },
+    'new-york-city': {
+      'timezone': 'America/New_York'
+    },
+    'london': {
+      'timezone': 'Europe/London'
+    },
+    'amsterdam': {
+      'timezone': 'Europe/Amsterdam'
+    },
+    'tokyo': {
+      'timezone': 'Asia/Tokyo'
+    },
+    'hong-kong': {
+      'timezone': 'Asia/Hong_Kong'
+    },
+    'sydney': {
+      'timezone': 'Australia/Sydney'
+    }
+  };
+
+  const time = new Date().toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit', timeZone: timezones[section].timezone });
+
+  return time;
+}
+
+function updateTime() {
+  const clocks = document.getElementsByClassName('clock');
+
+  Array.from(clocks).forEach(clock => {
+    let section = clock.getAttribute('data-section');
+    let newTime = setTime(section);
+
+    clock.innerText = newTime;
+  });
+}
+
+// Build navigation based on JSON list of cities
 async function buildCitiesNav() {
   const citiesData = await fetchCities();
   const cities = citiesData['cities'];
@@ -21,8 +62,11 @@ async function buildCitiesNav() {
   let html = '';
 
   cities.forEach(city => {
+    let time = setTime(city.section);
+
     let navItem = `<li class="nav__item">
                      <a href="#" class="nav__link">${ city.label }</a>
+                     <span class="clock" data-section="${ city.section }">${ time }</span>
                    </li>`;
 
     html += navItem;
@@ -98,4 +142,8 @@ async function setUpEvents() {
   });
 }
 
-buildCitiesNav().then(setUpEvents);
+buildCitiesNav()
+  .then(setUpEvents)
+  .then(() => { 
+    window.setInterval(updateTime, 1000); 
+  });
